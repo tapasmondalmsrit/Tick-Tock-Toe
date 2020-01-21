732dae36c57b97d8f2a8269f675acd2d542625cb
Tapas Mondal <mondaltapas02@gmail.com>
2020-01-21 06:33:24 +0530

    first commit

---
+++ jsgame.js
@@ -0,0 +1,67 @@
+
+let sign = "x";
+let disp = document.getElementById("player");
+
+
+function printx(number){
+    let box = document.getElementById("r"+number);
+  console.log(box);
+  
+  if(box.innerText==""){
+  box.innerText=sign;
+  winner();
+  checksign();
+ disp.innerHTML="<center>"+ sign + "  your turn "+"</center>";
+ 
+  }
+  
+  
+}
+function checksign(){
+    if(sign=="x")sign ="o";
+    else sign = "x";
+   
+}
+function getbox(no){
+    return document.getElementById("r"+no).innerHTML;
+}
+function checkmove(a,b,c,m){
+    if(getbox(a)==m && getbox(b)==m && getbox(c)==m)
+       return true;
+    else return false;   
+}
+
+function playagain(){
+    for(let i=1;i<=9;i++){
+        document.getElementById("r"+i).innerHTML="";
+    }
+    document.getElementById("playaginBtn").style.display = "none"
+    disp.innerHTML = "<center>" +sign + " your turn" + "</center>";
+    
+}
+
+function showplayagainbtn(){
+      document.getElementById("playaginBtn").style.display = "block";
+      document.getElementById("playaginBtn").onclick = playagain;
+}
+
+
+function winner(){
+if(checkmove(1,2,3,sign)||checkmove(4,5,6,sign)||checkmove(7,8,9,sign)
+    ||checkmove(1,4,7,sign)||checkmove(2,5,8,sign)||checkmove(3,6,9,sign)
+    ||checkmove(1,5,9,sign)||checkmove(7,5,3,sign)){
+    disp.innerHTML = "<center class='success'>" +sign + " is the  winner" + "</center>";
+    showplayagainbtn();
+   throw "game end";
+}else{
+    if(getbox(1)!=""&& getbox(2)!=""&& getbox(3)!=""&&
+   getbox(4)!=""&& getbox(5)!=""&& getbox(6)!=""&&
+   getbox(7)!=""&& getbox(8)!=""&& getbox(9)!=""){
+    
+    disp.innerHTML = "<center> its a tie </center>";
+    showplayagainbtn();
+       throw "its a tie";
+   }
+}
+}
+
